6632cff8e32b273697daa975507f48a0d721d273
Issai Mendoza <mendozaissai@gmail.com>
2023-01-04 13:01:18 -0600

    Remover codigo comentado

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -1,9 +1,7 @@
-import Header from "../layout/Header";
 import RunFry from "../assets/gif/run-fry-guy-run.gif";
 export default function ComingSoon() {
   return (
     <>
-      {/* <Header></Header> */}
       <main className="comingsoon">
         <figure className="comingsoon__container">
           <img className="comingsoon__image" src={RunFry} alt="Run Fry" />
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,84 +1,9 @@
 import Header from "../../layout/Header";
-//import "./home.scss";
 export default function Home() {
   return (
     <>
       <Header></Header>
-      {/* <main className="main">
-        <section className="home section">
-          <div className="home__container container">
-            <div className="home__content">
-              <div className="home__social">
-                <a
-                  href="https://www.linkedin.com/in/issaimv/"
-                  target="_new"
-                  className="home__social-icon"
-                >
-                  <i className="uil uil-linkedin-alt"></i>
-                </a>
-                <a
-                  href="https://github.com/IssaiMV/"
-                  target="_new"
-                  className="home__social-icon"
-                >
-                  <i className="uil uil-github"></i>
-                </a>
-              </div>
-              <div className="home__img">
-                <svg
-                  className="home__blob"
-                  viewBox="0 0 200 187"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                >
-                  <mask id="mask0" mask-type="alpha">
-                    <path
-                      d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
-        130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
-        97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
-        0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
-                    />
-                  </mask>
-                  <g mask="url(#mask0)">
-                    <path
-                      d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
-        165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
-        129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
-        -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
-                    />
-                    <image
-                      className="home__blob-img"
-                      x={12}
-                      y={18}
-                      xlinkHref="https://i.imgur.com/w7GCRPb.png"
-                    />
-                  </g>
-                </svg>
-              </div>
-              <div className="home__data">
-                <h1 className="home__title">Hi, I'm Issai</h1>
-                <h3 className="home__subtitle">Fullstack developer</h3>
-                <p className="home__description">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Quasi nulla architecto fuga dicta necessitatibus eligendi
-                  laborum odit, voluptates sint, provident alias animi nesciunt
-                  quaerat, facilis maxime corrupti! Pariatur, a quisquam!
-                </p>
-                <a href="#contact" className="button button--flex">
-                  Contact Me<i className="uil uil-message button__icon"></i>
-                </a>
-              </div>
-            </div>
-            <div className="home__scrooll">
-              <button className="home_scroll-button button--flex">
-                <i className="uil uil-mouse-alt home__scroll-mouse"></i>
-                <span>Scroll down</span>
-                <i className="uil uil-arrow-down home__scroll-arrow"></i>
-              </button>
-            </div>
-          </div>
-        </section>
-      </main> */}
+
     </>
   );
 }
